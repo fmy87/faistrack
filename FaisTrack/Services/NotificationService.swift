cfc745e47eb33cf32015c61b94d46649af3e381c
FaisTrack <fmy87@hotmail.com>
2026-06-23 17:59:13 +0000

    Fix NotificationService: wrap await in Task inside non-async func

--- FaisTrack/Services/NotificationService.swift
+++ FaisTrack/Services/NotificationService.swift
@@ -25,7 +25,7 @@ class NotificationService {
         content.body = NSLocalizedString("notification.driveStart.body", comment: "")
         content.sound = .default
         let request = UNNotificationRequest(identifier: "driveStart", content: content, trigger: nil)
-        try? await UNUserNotificationCenter.current().add(request)
+        Task { try? await UNUserNotificationCenter.current().add(request) }
     }
 
     func sendDriveEndNotification(drive: Drive) async {
